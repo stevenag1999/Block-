cc005f8543e5cd885bff381b529d4d50e2b9cd5d
stevenag1999 <92649989+stevenag1999@users.noreply.github.com>
2025-11-14 23:11:34 -0600

    Update common_bfp.h

--- SW/common_bfp.h
+++ SW/common_bfp.h
@@ -1,6 +1,6 @@
 /**
  * Common definitions for BFP HW/SW interface
- * Shared between host application and FPGA kernel
+ * ADAPTED for compact format with delta (HW: WE=5, WM=7)
  */
 
 #ifndef COMMON_BFP_H
@@ -12,6 +12,9 @@
 #define WE 5
 #define WM 7
 #define N  16
+
+// Compact format size: 1 exp_shared + 3*N (sign, mant, delta per element)
+#define BFP_BLOCK_SIZE (1 + 3 * N)  // 49 for N=16
 
 // Operation codes - Must match bfp_kernel.cpp enum
 typedef enum : unsigned int {
@@ -34,5 +37,44 @@ static const char* OP_NAMES[] = {
     "DIV",
     "RCP"
 };
+
+// Helper: Pack BFP data into compact format for HW
+// Format: [exp_shared, sign[0], mant[0], delta[0], sign[1], mant[1], delta[1], ...]
+inline void pack_bfp_to_compact(
+    uint32_t exp_shared,
+    const uint32_t* sign,
+    const uint32_t* mant,
+    const uint32_t* delta,
+    uint32_t* compact_buf,
+    uint32_t offset
+) {
+    compact_buf[offset] = exp_shared;
+    uint32_t idx = offset + 1;
+    
+    for (int i = 0; i < N; i++) {
+        compact_buf[idx++] = sign[i];
+        compact_buf[idx++] = mant[i];
+        compact_buf[idx++] = delta[i];
+    }
+}
+
+// Helper: Unpack compact format from HW to separate arrays  
+inline void unpack_compact_to_bfp(
+    const uint32_t* compact_buf,
+    uint32_t offset,
+    uint32_t& exp_shared,
+    uint32_t* sign,
+    uint32_t* mant,
+    uint32_t* delta
+) {
+    exp_shared = compact_buf[offset];
+    uint32_t idx = offset + 1;
+    
+    for (int i = 0; i < N; i++) {
+        sign[i]  = compact_buf[idx++];
+        mant[i]  = compact_buf[idx++];
+        delta[i] = compact_buf[idx++];
+    }
+}
 
 #endif // COMMON_BFP_H
